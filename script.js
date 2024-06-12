7cdc823bd05f1d6297c49219927ce589b499f36a
Andrés Murillo <andresdavid1305@hotmail.com>
2024-06-12 16:47:37 -0500

    mi segundo commit

--- script.js
+++ script.js
@@ -3,17 +3,21 @@
 let menuIcon = document.querySelector('#menu-icon');
 let navbar = document.querySelector('.navbar');
 
-menuIcon = onclick = () =>{
+
+menuIcon.addEventListener("click", () =>{
+
     menuIcon.classList.toggle('bxs-x-circle');
     navbar.classList.toggle('active');
-};
+  });
+
+
 
 
 let sections = document.querySelectorAll('section');
 let navLinks = document.querySelectorAll('header nav a');
 
 window.onscroll = () => {
-    sections.map(sec =>{
+    sections.forEach(sec =>{
         let top = window.scrollY;
         let offset = sec.offsetTop - 150;
         let height = sec.offsetHeight;
@@ -21,7 +25,7 @@ window.onscroll = () => {
 
 
         if(top >= offset && top < offset + height){
-            navLinks.map(links =>{
+            navLinks.forEach(links =>{
                 links.classList.remove('active');
                 document.querySelector('header nav a[href*=' +  id  + ']').classList.add('active');
             });
@@ -36,8 +40,32 @@ window.onscroll = () => {
     header.classList.toggle('sticky', window.scrollY > 100);
 
 
-    /* remoe toggle and navbar when  click navbar*/
+    /* remove toggle and navbar when  click navbar*/
     menuIcon.classList.remove('bxs-x-circle');
     navbar.classList.remove('active');
 
 };
+
+/* */
+ScrollReveal({ 
+    reset: true,
+    distance:'80px',
+    duration: 2000,
+    delay:200
+});
+
+ScrollReveal().reveal('.home-content, .heading',  { origin: 'top' });
+ScrollReveal().reveal('.home-img, .services-container, portfolio-container, contact-form',  { origin: 'bottom' });
+ScrollReveal().reveal('.home-content h1, .about-img',  { origin: 'left' });
+ScrollReveal().reveal('.home-content p, .about-content',  { origin: 'right' });
+
+
+/**typed js */
+
+let typed = new typed('#multiple-text',{
+    strings:['Frontend Developer', 'Youtuber', 'Blogger'],
+    typeSpeed:100,
+    backSpeed:100,
+    backDelay:1000,
+    loop:true
+});
