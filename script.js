8b7a60c314038103ca9480e834ab38a9c08b5270
Andrés Murillo <andresdavid1305@hotmail.com>
2024-06-12 14:14:42 -0500

    mi primer commit

---
+++ script.js
@@ -0,0 +1,43 @@
+// ==================== toggle icon-navbar ==================
+
+let menuIcon = document.querySelector('#menu-icon');
+let navbar = document.querySelector('.navbar');
+
+menuIcon = onclick = () =>{
+    menuIcon.classList.toggle('bxs-x-circle');
+    navbar.classList.toggle('active');
+};
+
+
+let sections = document.querySelectorAll('section');
+let navLinks = document.querySelectorAll('header nav a');
+
+window.onscroll = () => {
+    sections.map(sec =>{
+        let top = window.scrollY;
+        let offset = sec.offsetTop - 150;
+        let height = sec.offsetHeight;
+        let id = sec.getAttribute('id');
+
+
+        if(top >= offset && top < offset + height){
+            navLinks.map(links =>{
+                links.classList.remove('active');
+                document.querySelector('header nav a[href*=' +  id  + ']').classList.add('active');
+            });
+        };
+
+    });
+
+    // ========================= Sticky navbar =====================================
+    
+    let header = document.querySelector('header');
+
+    header.classList.toggle('sticky', window.scrollY > 100);
+
+
+    /* remoe toggle and navbar when  click navbar*/
+    menuIcon.classList.remove('bxs-x-circle');
+    navbar.classList.remove('active');
+
+};
